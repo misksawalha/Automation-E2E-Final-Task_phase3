d29f4f2079c9bd3c281af6b7cb32f07858a64f32
unknown <misksawalha>
2023-11-15 13:25:02 +0200

    uploade video

--- cypress/support/init/createVacancyInit.ts
+++ cypress/support/init/createVacancyInit.ts
@@ -4,10 +4,10 @@ import { vacancyVar } from "../variable/createVacancy.var";
 import { jobTitleId } from "../../support/helpers/jobTitleHelper";
 
 export default class CreateVacancyInit {
-    static initCreateVacancy(empID:Number): CreateVacancyPayload {
+    static initCreateVacancy(empID:Number,i:any): CreateVacancyPayload {
         let createCreateVacancyPayload: CreateVacancyPayload = {
             Vacancy: {
-                name: vacancyVar.vacancyname,
+                name: vacancyVar.vacancyname+i,
                 jobTitleId: jobTitleId,
                 employeeId: empID,
                 numOfPositions: null,
